baaade3441e558cdc866ab43bfb63b4004357ba5
Michal Tichý <michal.tichy@riganti.cz>
2017-12-14 19:33:31 +0100

    Typo fixed

--- VutLogin_BranLove_Converter/BrainLoveCodeGenerator.cs
+++ VutLogin_BranLove_Converter/BrainLoveCodeGenerator.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace VutLogin_BranLove_Converter
+namespace VutLogin_BrainLove_Converter
 {
     public static class BrainLoveCodeGenerator
     {
--- VutLogin_BranLove_Converter/VutLoginTranslator.cs
+++ VutLogin_BranLove_Converter/VutLoginTranslator.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace VutLogin_BranLove_Converter
+namespace VutLogin_BrainLove_Converter
 {
    public static class VutLoginTranslator
     {
